ae15fe95b411c1b253ac3ed1510334838c6c015d
Niklas T <niklast@pop-os.localdomain>
2023-05-11 17:17:07 +0200

    added files for bluerov control, viz and bringup, bridge clock topic

--- hippo_sim/launch/spawn_vehicle.launch.py
+++ hippo_sim/launch/spawn_vehicle.launch.py
@@ -25,6 +25,9 @@ def generate_launch_description():
         default_value=default_vehicle_name,
         description='Vehicle name used as namespace.')
     fake_estimator_launch_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value=str(False),
+        description='Vehicle name used as namespace.')
         name='fake_state_estimation', default_value='false')
     fake_vision_launch_arg = DeclareLaunchArgument(name='fake_vision',
                                                    default_value='false')
@@ -112,6 +115,7 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         model_launch_arg,
+        use_sim_time_launch_arg,
         vehicle_name_launch_arg,
         fake_estimator_launch_arg,
         fake_vision_launch_arg,
