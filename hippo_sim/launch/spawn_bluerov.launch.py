c49c5497153a1d8d567f78cb67d71ff02ffaeca8
Thies Lennart Alff <lennart.alff@gmail.com>
2023-10-10 16:14:14 +0000

    clenaup

--- hippo_sim/launch/spawn_bluerov.launch.py
+++ hippo_sim/launch/spawn_bluerov.launch.py
@@ -1,14 +1,16 @@
 from ament_index_python.packages import get_package_share_path
-import launch
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
-def generate_launch_description():
+def generate_launch_description() -> LaunchDescription:
     package_path = get_package_share_path('hippo_sim')
-    launch_path = str(package_path / 'launch/spawn_vehicle.launch.py'),
+    launch_path = str(package_path / 'launch/spawn_vehicle.launch.py')
     model_path = str(package_path / 'models/bluerov/urdf/bluerov.xacro')
 
-    vehicle_spawner = launch.actions.IncludeLaunchDescription(
-        launch.launch_description_sources.PythonLaunchDescriptionSource(
+    vehicle_spawner = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
             launch_path),
-        launch_arguments=dict(model_path=model_path).items())
-    return launch.LaunchDescription([vehicle_spawner])
+        launch_arguments={'model_path': model_path}.items())
+    return LaunchDescription([vehicle_spawner])
