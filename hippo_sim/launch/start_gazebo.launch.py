da93f57d54dd73a9832a7af5e67e409f4123fe63
Thies Lennart Alff <lennart.alff@gmail.com>
2023-10-12 07:51:05 +0000

    restructured, more configurable

--- hippo_sim/launch/start_gazebo.launch.py
+++ hippo_sim/launch/start_gazebo.launch.py
@@ -1,71 +1,97 @@
-import launch
-import launch_ros
 from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    ExecuteProcess,
+)
+from launch.conditions import IfCondition
+from launch.events import Shutdown
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
 
 
-def generate_launch_description():
+def declare_launch_args(launch_description: LaunchDescription) -> None:
+    action = DeclareLaunchArgument(name='start_gui',
+                                   default_value='false',
+                                   description='Start the gazebo GUI.')
+    launch_description.add_action(action)
+
     package_path = get_package_share_path('hippo_sim')
-    world = package_path / 'models' / 'world' / 'empty.sdf'
+    world_file = str(package_path / 'models' / 'world' / 'empty.sdf')
+    action = DeclareLaunchArgument(name='world_file', default_value=world_file)
+    launch_description.add_action(action)
+
+
+def create_gazebo_action() -> ExecuteProcess:
+    return ExecuteProcess(
+        cmd=[
+            'ign',
+            'gazebo',
+            '-v 2',
+            '-r',
+            '-s',
+            '--headless-rendering',
+            LaunchConfiguration('world_file'),
+        ],
+        output='screen',
+        on_exit=Shutdown(),
+    )
+
+
+def create_gazebo_gui_action() -> ExecuteProcess:
+    return ExecuteProcess(cmd=['ign', 'gazebo', '-g', '-v 1'],
+                          condition=IfCondition(
+                              LaunchConfiguration('start_gui')),
+                          output='log',
+                          on_exit=Shutdown())
+
+
+def create_spawn_pool_action() -> Node:
+    package_path = get_package_share_path('hippo_sim')
     pool_path = package_path / 'models/pool/urdf/pool.xacro'
-
-    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time',
-                                                            default=True)
-
-    pool_description = launch.substitutions.LaunchConfiguration(
+    pool_description = LaunchConfiguration(
         'pool_description',
-        default=launch.substitutions.Command([
+        default=Command([
             'ros2 run hippo_sim create_robot_description.py ',
             '--input ',
             str(pool_path),
         ]))
     pool_params = {'pool_description': pool_description}
+    return Node(package='hippo_sim',
+                executable='spawn',
+                parameters=[pool_params],
+                arguments=[
+                    '--param',
+                    'pool_description',
+                    '--x',
+                    '1.0',
+                    '--y',
+                    '2.0',
+                    '--z',
+                    '-1.5',
+                ],
+                output='screen')
 
-    gazebo = launch.actions.ExecuteProcess(
-        cmd=[
-            'ign',
-            'gazebo',
-            '-v 3',
-            '-r',
-            '-s',
-            '--headless-rendering',
-            str(world),
-        ],
-        output='screen',
-    )
 
-    return launch.LaunchDescription([
-        gazebo,
-        launch_ros.actions.Node(package='hippo_sim',
-                                executable='spawn',
-                                parameters=[pool_params],
-                                arguments=[
-                                    '--param',
-                                    'pool_description',
-                                    '--x',
-                                    '1.0',
-                                    '--y',
-                                    '2.0',
-                                    '--z',
-                                    '-1.5',
-                                ],
-                                output='screen'),
-        # bridge the clock topic
-        launch_ros.actions.Node(
-            name='clock_bridge',
-            package='ros_gz_bridge',
-            executable='parameter_bridge',
-            arguments=[
-                '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
-            ],
-            parameters=[
-                {
-                    'use_sim_time': use_sim_time,
-                },
-            ],
-            output='screen'),
-        launch.actions.
-        RegisterEventHandler(event_handler=launch.event_handlers.OnProcessExit(
-            target_action=gazebo,
-            on_exit=[launch.actions.EmitEvent(
-                event=launch.events.Shutdown())])),
-    ])
+def create_clock_bridge_action() -> Node:
+    return Node(name='clock_bridge',
+                package='ros_gz_bridge',
+                executable='parameter_bridge',
+                arguments=[
+                    '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+                ],
+                output='screen')
+
+
+def generate_launch_description():
+    launch_description = LaunchDescription()
+    declare_launch_args(launch_description=launch_description)
+    actions = [
+        create_spawn_pool_action(),
+        create_clock_bridge_action(),
+        create_gazebo_action(),
+        create_gazebo_gui_action(),
+    ]
+    for action in actions:
+        launch_description.add_action(action)
+    return launch_description
