2348cfdacfa27f715124f07ce09e735a042db92f
Thies Lennart Alff <lennart.alff@gmail.com>
2023-11-29 14:14:18 +0000

    updated tag position for range tags

--- hippo_sim/launch/spawn_wall_tags.launch.py
+++ hippo_sim/launch/spawn_wall_tags.launch.py
@@ -13,22 +13,22 @@ def generate_launch_description() -> LaunchDescription:
     tmp = math.pi / 2.0
     poses = [
         {
-            'xyz': (0.7, 4.0, -0.5),
+            'xyz': (0.7, 3.8, -0.5),
             'rpy': (tmp, 0.0, 0.0),
             'tag_id': 0,
         },
         {
-            'xyz': (1.3, 4.0, -0.5),
+            'xyz': (1.3, 3.8, -0.5),
             'rpy': (tmp, 0.0, 0.0),
             'tag_id': 1,
         },
         {
-            'xyz': (0.7, 4.0, -0.9),
+            'xyz': (0.7, 3.8, -0.9),
             'rpy': (tmp, 0.0, 0.0),
             'tag_id': 2,
         },
         {
-            'xyz': (1.3, 4.0, -0.9),
+            'xyz': (1.3, 3.8, -0.9),
             'rpy': (tmp, 0.0, 0.0),
             'tag_id': 3,
         },
